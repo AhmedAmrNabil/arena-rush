8803417075950d7e3bef7fe6ba5dd4cb4c493c5e
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-02-19 01:50:22 +0200

    feat: move to vendored submodule libraries for cross compatibility

--- flake.nix
+++ flake.nix
@@ -15,74 +15,68 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        packageName = "project-opengl-app";
         pkgs = import nixpkgs { inherit system; };
+        buildInputs = with pkgs; [
+          # libraries
+          libGL
+
+          # Wayland
+          wayland
+          wayland-protocols
+          libxkbcommon
+          egl-wayland
+          libdecor
+          wayland-scanner
+
+          # X11
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libXinerama
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+          pkg-config
+          makeWrapper
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
+
+          inherit buildInputs nativeBuildInputs;
+
           packages = with pkgs; [
-
-            # build tools
-            cmake
-            ninja
-            gcc
-            pkg-config
-
             # helper
             just
-
-            # libraries
-            glfw
-            glm
-            libGL
-            tinyobjloader
-
-            # Display protocols
-            # wayland
-            wayland
-            wayland-protocols
-            libxkbcommon
-            egl-wayland
-            libdecor
-
-            # X11
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libXinerama
           ];
+
+          shellHook = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib";
         };
 
         packages = {
           default = self.packages.${system}.opengl_app;
           opengl_app = pkgs.stdenv.mkDerivation {
-            pname = "opengl_app";
+            pname = packageName;
             version = "1.0.0";
 
             src = self;
 
-            nativeBuildInputs = with pkgs; [
-              cmake
-              ninja
-              pkg-config
-            ];
-
-            buildInputs = with pkgs; [
-              glfw
-              glm
-              libGL
-              tinyobjloader
-
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXrandr
-              xorg.libXi
-              xorg.libXinerama
-            ];
+            inherit buildInputs nativeBuildInputs;
 
             cmakeFlags = [
               "-DCMAKE_BUILD_TYPE=Release"
             ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ./opengl_app $out/bin/
+              wrapProgram $out/bin/opengl_app \
+                --set LD_LIBRARY_PATH ${pkgs.libGL}/lib:${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH
+            '';
           };
         };
       }
