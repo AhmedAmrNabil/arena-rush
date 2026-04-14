9f7d8e0dfd59cd690db67cb674b5ad394a4f5614
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-04-15 01:35:51 +0200

    feat: add bone weight and id to vertex

--- src/common/mesh/mesh.hpp
+++ src/common/mesh/mesh.hpp
@@ -11,6 +11,8 @@ namespace our {
 #define ATTRIB_LOC_TEXCOORD 2
 #define ATTRIB_LOC_NORMAL 3
 #define ATTRIB_LOC_TANGENT 4
+#define ATTRIB_LOC_BONE_IDS 5
+#define ATTRIB_LOC_BONE_WEIGHTS 6
 
     class Mesh {
         // Here, we store the object names of the 3 main components of a mesh:
@@ -19,8 +21,16 @@ namespace our {
         unsigned int VAO;
         // We need to remember the number of elements that will be draw by glDrawElements
         GLsizei elementCount;
+        GLsizei vertexCount;
 
     public:
+        GLsizei getVertexCount() const {
+            return vertexCount;
+        }
+
+        GLsizei getIndexCount() const {
+            return elementCount;
+        }
         // The constructor takes two vectors:
         // - vertices which contain the vertex data.
         // - elements which contain the indices of the vertices out of which each rectangle will be constructed.
@@ -59,6 +69,14 @@ namespace our {
                                   (void*)offsetof(Vertex, tangent));
             glEnableVertexAttribArray(ATTRIB_LOC_TANGENT);
 
+            glVertexAttribPointer(ATTRIB_LOC_BONE_IDS, 4, GL_INT, GL_FALSE, sizeof(Vertex),
+                                  (void*)offsetof(Vertex, bone_ids));
+            glEnableVertexAttribArray(ATTRIB_LOC_BONE_IDS);
+
+            glVertexAttribPointer(ATTRIB_LOC_BONE_WEIGHTS, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                                  (void*)offsetof(Vertex, weights));
+            glEnableVertexAttribArray(ATTRIB_LOC_BONE_WEIGHTS);
+
             // Generate EBO and send indices of vertices
             glGenBuffers(1, &EBO);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -71,6 +89,7 @@ namespace our {
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
             elementCount = static_cast<GLsizei>(elements.size());
+            vertexCount = static_cast<GLsizei>(vertices.size());
         }
 
         // this function should render the mesh
