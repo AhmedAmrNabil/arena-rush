6bbcfd3d284a75f3c77282453df83a332f96f25a
Ahmed Sobhy <48056730+AhmedSobhy01@users.noreply.github.com>
2026-04-17 21:21:49 +0200

    refactor: remove radius and height from player component

--- src/game/components/player.cpp
+++ src/game/components/player.cpp
@@ -3,9 +3,7 @@
 namespace gameplay {
 
     void PlayerComponent::deserialize(const nlohmann::json& data) {
-        if (!data.is_object()) return;
-        radius = data.value("radius", radius);
-        height = data.value("height", height);
+        //
     }
 
 }  // namespace gameplay
